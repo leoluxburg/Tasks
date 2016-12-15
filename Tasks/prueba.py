3dd215a2f1e2f7de6cc98878a55fab823d619534
albertovillar04 <alberto.villar@outlook.com>
2016-12-14 20:22:58 -0500

    Add files via upload

---
+++ Tasks/prueba.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class prueba(unittest.TestCase):
+    def initialize_db(self):
+        r = teardown_request
+        assertEqual(r, msg=Flase)
+
+
+if __name__ == "__main__":
+    unittest.main()
