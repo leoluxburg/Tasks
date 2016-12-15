1e0f80d6aadbff1f5385a84d89ffe3dcab32013c
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2016-12-14 20:11:40 -0500

    ready
    kjhgfds

--- Tasks/app.py
+++ Tasks/app.py
@@ -2,30 +2,35 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import *
 
 app = Flask(__name__ ,static_url_path='/static')
-
 @app.before_request
 def before_request():
-	initialize_db()
+	"""Abre la base de datos"""
 
+	initialize_db()
 
 @app.teardown_request
 def teardown_request(exception):
+	"""Cierra la base de datos"""
 	db.close()
 
 @app.route('/')
 def home():
-	return render_template('home.html') 
+	"""Homepage"""
+	return render_template('home.html')
 
 @app.route('/tasks/')
 def tasks():
-	return render_template('tasks.html',posts=Post.select().order_by(Post.date)) 
+	"""Tasks Pages"""
+	return render_template('tasks.html',posts=Post.select().order_by(Post.date))
 
 @app.route('/new_post/')
 def new_post():
+	"""Creacion de tasks"""
 	return render_template('new_post.html')
 
 @app.route('/create/',methods = ['POST'])
 def create_post():
+	"""Metodo para la creacion de los tasks"""
 	Post.create(
 		title = request.form['title'],
 		tasks = request.form['tasks'],
@@ -34,5 +39,3 @@ def create_post():
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
-
--- Tasks/models.py
+++ Tasks/models.py
@@ -1,6 +1,5 @@
 from peewee import *
 import datetime
-
 db= SqliteDatabase('posts.db')
 
 class Post(Model):
@@ -8,12 +7,13 @@ class Post(Model):
 	date = DateTimeField(default = datetime.datetime.now)
 	title = CharField()
 	tasks = TextField()
+	""" Peewee """
 
 	class Meta:
 		database = db
 
 def initialize_db():
+	""" La creacion de la abase de datos"""
+
 	db.connect()
 	db.create_tables([Post], safe = True)
-
-	
